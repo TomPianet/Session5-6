bfb863c520136ee055e5a7e32cb9c71f06830076
Tom Pianet <tom.pianet.us@gmail.com>
2024-02-02 12:17:07 +0100

    t

--- exception.py
+++ exception.py
@@ -17,9 +17,37 @@ except ValueError:
 except ZeroDivisionError:
     print("you cannot divide by zero")
 
+except:
+    print("some other error i did not foresee")
+
 else:
     print("no exceptions were raised")
 
 finally:
     print("thank you for playing")
 
+try:
+    name = input("What is your name?")
+    age = input("How old are you?")
+    age = int(age) #convert string to integer
+
+
+except ValueError:
+    print("Invalid age. Please enter a number.")
+else:
+
+if age < 0 or age > 140
+    drinks = ("vodka", "tequila", "gin")
+
+elif age > 120:
+        print("You are too old to play the awesome drinking game.")
+
+elif age < 18:
+        print("You are a minor. You can not play the awesome drinking game.")
+
+elif (country == "USA" or country == "UAE") and age < 21
+        print( )
+
+else:
+        print("You are an adult. You can play the awesome drinking game.")
+        print("Have some", random.choice(drinks), "and enjoy the game."
