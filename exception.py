1f655d03340b37bd9fa3fec8a68a9e3fbe11ef56
Tom Pianet <tom.pianet.us@gmail.com>
2024-02-02 11:08:22 +0100

    t

--- exception.py
+++ exception.py
@@ -7,6 +7,9 @@ try:
     age = int(age) # convert string to integer
     birth_year = 2023 - age
     print(name, " you were born in ", birth_year, ".", sep="")
+    number = input("give me a number to divide the age")
+    number = int(number)
+    print(age / number)
 
 except:
     print("invalid age. Please enter a number")
