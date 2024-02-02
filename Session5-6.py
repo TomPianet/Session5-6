c6b6db2382196fc4a465475b761f7593fa05184d
Tom Pianet <tom.pianet.us@gmail.com>
2024-02-02 11:03:54 +0100

    t

--- Session5-6.py
+++ Session5-6.py
@@ -5,6 +5,6 @@ print("Hello, " , name, "!", sep="" )
 
 age = int(age) # convert string to integer
 
-birth_year = 2024 - age
+birth_year = 2023 - age
 print(name, " you were born in ", birth_year, ".", sep="")
 
